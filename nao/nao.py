7121d73bbab94b6fdd4133a941e28e31e80a76c8
章人 東間 <akito.tohma@gmail.com>
2020-01-22 16:15:24 +0900

    プロジェクト ファイルを追加します。

---
+++ nao/nao.py
@@ -0,0 +1,9 @@
+from naoqi import ALProxy
+tts = ALProxy("ALTextToSpeech", "localhost", 52836)
+tts.say("Hello, world!")
+
+tts.say("Hello, world!")
+tts.say("Hello, world!")
+tts.say("Hello, world!")
+tts.say("Hello, world!")
+tts.say("Hello, world!")
